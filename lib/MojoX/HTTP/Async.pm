5aab68f8067d105e8aafffefcfb28b51c4406636
Chernenko Dmitriy <d.chernenko@corp.mail.ru>
2021-12-23 11:40:34 +0300

    v0.13

--- lib/MojoX/HTTP/Async.pm
+++ lib/MojoX/HTTP/Async.pm
@@ -88,7 +88,7 @@ use URI ();
 use Scalar::Util qw/ blessed /;
 use Errno qw / :POSIX /;
 
-our $VERSION = 0.12;
+our $VERSION = 0.13;
 
 use constant {
     IS_WIN     => ($^O eq 'MSWin32') ? 1 : 0,
